311e7c06996a9c6ead02a06d5bc3b95027e817d1
Henry <67282986+hungtp2912@users.noreply.github.com>
2022-12-14 17:13:36 -0600

    Done all matrix

--- Matrix/src/main/java/IMatrix.java
+++ Matrix/src/main/java/IMatrix.java
@@ -2,12 +2,12 @@ public interface IMatrix {
     IMatrix Add(IMatrix mRight) throws IllegalArgumentException;
     IMatrix Multiply(IMatrix mRight) throws  IllegalArgumentException;
     IMatrix ScalarMultiply(double dScalar);
-    IMatrix Substract(IMatrix mRight) throws  IllegalArgumentException;
+    IMatrix Subtract(IMatrix mRight) throws  IllegalArgumentException;
 
     IMatrix Scaling(double dX, double dY);
     IMatrix Translate(double dX, double dY);
     IMatrix Rotate(double radians);
-    IMatrix Rotatee(double radians, double dOx, double dOy);
+    IMatrix Rotate(double radians, double dOx, double dOy);
     IMatrix leastSquare(int m);
     IMatrix gaussJordanElimination();
 }
